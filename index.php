31e153f5498ffb069d2bdba5a99fd6e299477468
andrea-salvalaggio <andrea.salvalaggio95@gmail.com>
2022-08-24 16:08:36 +0200

    quarto paragrafo completato

--- index.php
+++ index.php
@@ -82,7 +82,31 @@
                                 </li>
                              </ol>
                              Tieni presente che i servizi Google sono fondamentalmente gli stessi a prescindere dalla società
-                             consociata che li offre o dal paese a cui è associato il tuo account.',
+                             consociata che li offre o dal paese a cui è associato il tuo account.
+                             <br><br><br>
+                             <h5>Stabilire il paese associato al tuo account</h5>
+                             Quando crei un nuovo Account Google, lo associamo a un paese in base a dove è stato creato.
+                             Per quanto riguarda gli account creati almeno un anno fa, usiamo il paese da cui accedi solitamente ai servizi Google,
+                             in genere i servizi in cui hai trascorso più tempo nell ultimo anno.
+                             <br><br>
+                             I viaggi frequenti solitamente non influiscono sul paese associato al tuo account.
+                             Se ti trasferisci in un altro paese, potrebbe occorrere circa un anno per aggiornare l associazione del paese.
+                             <br><br>
+                             Se il paese associato al tuo account non corrisponde al tuo paese di residenza, il motivo potrebbe essere la differenza
+                             tra il paese in cui lavori e il paese in cui risiedi, l installazione di una rete privata virtuale (VPN) per mascherare
+                             il tuo indirizzo IP oppure la residenza vicino a un confine territoriale. Contattaci se ritieni che il paese associato
+                             al tuo account sia sbagliato.'
+            ],
+            [
+                "question" => "Come faccio a rimuovere informazioni su di me dai risultati di ricerca di Google?",
+                "answer" => "I risultati di ricerca di Google rispecchiano i contenuti pubblicamente disponibili sul Web. I motori di ricerca non
+                             possono rimuovere i contenuti direttamente dai siti web, quindi rimuovere risultati di ricerca da Google non consente
+                             di rimuovere i contenuti dal Web. Se desideri rimuovere qualcosa dal Web, devi contattare il webmaster del sito su
+                             cui sono pubblicati i contenuti e chiedergli di apportare una modifica. Inoltre, se, ai sensi delle leggi europee
+                             per la protezione dei dati, desideri richiedere la rimozione di determinate informazioni su di te visualizzate nei
+                             risultati di ricerca di Google, fai clic qui. Una volta che i contenuti saranno stati rimossi e che Google avrà rilevato
+                             l'aggiornamento, le informazioni non verranno più visualizzate nei risultati di ricerca di Google. In caso di una richiesta
+                             di rimozione urgente, è inoltre possibile visitare la nostra pagina di assistenza per avere ulteriori informazioni.",
             ],
             
         ];
