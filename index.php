306d7fbd91c4de20267e0183d8a46dd78ed2cf1a
andrea-salvalaggio <andrea.salvalaggio95@gmail.com>
2022-08-24 15:19:44 +0200

    primo paragrafo completato

--- index.php
+++ index.php
@@ -19,18 +19,44 @@
                 "answer" => "La recente decisione della Corte di giustizia dell'Unione europea ha profonde conseguenze per i motori di ricerca in Europa.
                              La Corte ha stabilito che alcuni utenti hanno il diritto di chiedere ai motori di ricerca come Google di rimuovere risultati
                              relativi a chiavi di ricerca che includono il proprio nome. Per poter essere rimossi, i risultati visualizzati devono essere 
-                             inadeguati, irrilevanti o non più rilevanti, o eccessivi.",
+                             inadeguati, irrilevanti o non più rilevanti, o eccessivi.
+                             <br><br>
+                             Da quando questa decisione è stata pubblicata il 13 maggio 2014, abbiamo lavorato incessantemente per uniformarci a essa.
+                             Si tratta di una procedura complessa perché dobbiamo valutare ogni singola richiesta e effettuare un bilanciamento tra il
+                             diritto dell'individuo a controllare i suoi dati personali ed il diritto di tutti di conoscere e distribuire le informazioni.
+                             <br><br>
+                             Per presentare una richiesta di rimozione, compila questo modulo web. Riceverai una risposta automatica che conferma la
+                             ricezione della tua richiesta. Dopodiché valuteremo il tuo caso (tieni presente che potrebbe occorrere un po' di tempo perché
+                             abbiamo già ricevuto tante richieste simili). Nel valutare la richiesta, considereremo se i risultati includono informazioni obsolete
+                             relative alla tua vita privata. Considereremo inoltre se vi è un interesse pubblico a che le informazioni rimangano nei nostri risultati
+                             delle ricerche, ad esempio se riguardano frodi finanziarie, negligenza professionale, condanne penali o la tua condotta pubblica in
+                             relazione a un pubblico ufficio (eletto o non eletto). Queste valutazioni sono complesse e, in quanto organizzazione privata, potremmo
+                             non essere nella posizione giusta per prendere decisioni in merito al tuo caso. Se non sei d'accordo con la nostra valutazione, puoi
+                             rivolgerti all'Autorità garante per la protezione dei dati personali nel tuo paese.
+                             <br><br>
+                             Nei prossimi mesi lavoreremo a stretto contatto con le autorità per la protezione dei dati e con altre autorità per perfezionare
+                             il nostro approccio. La decisione della CGUE rappresenta un cambiamento significativo per i motori di ricerca. Siamo preoccupati per
+                             le sue conseguenze, ma riteniamo anche che sia importante rispettare la decisione della Corte e ci stiamo adoperando per predisporre
+                             una procedura conforme alla legge.
+                             <br><br>
+                             Quando cerchi un nome, potresti trovare una notifica in cui viene spiegato che i risultati potrebbero essere stati modificati nel rispetto
+                             delle leggi europee per la protezione dei dati. Mostriamo questa notifica agli utenti europei quando cercano la maggior parte dei nomi,
+                             non soltanto le pagine che sono state soggette a una rimozione.",
             ],
+            
         ];
     ?>
 
     <div class="container">
         <div class="row">
-            <div class="col-12 mt-5">
+            <div class="col-12">
                 <?php foreach ($dataBase as $data) { ?>
-                    <h3>
+                    <h3 class="mt-5 mb-4">
                         <?php echo $data['question']?>
                     </h3>
+                    <p>
+                        <?php echo $data['answer']?>
+                    </p>
                 <?php } ?>
             </div>
         </div>
