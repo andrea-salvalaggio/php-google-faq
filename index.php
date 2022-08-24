4123a38a1ab399a0a2efbd02e9b9d22e8e0e32db
andrea-salvalaggio <andrea.salvalaggio95@gmail.com>
2022-08-24 16:58:44 +0200

    quinto paragrafo completato

--- index.php
+++ index.php
@@ -108,7 +108,18 @@
                              l'aggiornamento, le informazioni non verranno più visualizzate nei risultati di ricerca di Google. In caso di una richiesta
                              di rimozione urgente, è inoltre possibile visitare la nostra pagina di assistenza per avere ulteriori informazioni.",
             ],
-            
+            [
+                "question" => "Quando faccio clic sui risultati della Ricerca Google, le mie chiavi di ricerca vengono inviate ai siti web?",
+                "answer" => "In alcuni casi sì. Quando fai clic su un risultato della Ricerca Google, il tuo browser web potrebbe reindirizzare
+                             alla pagina web di destinazione anche l'indirizzo Internet, o URL, della pagina dei risultati di ricerca sotto forma
+                             di URL referrer. Talvolta, l'URL della pagina dei risultati di ricerca potrebbe contenere la query di ricerca che hai
+                             inserito. Se utilizzi la ricerca SSL (la funzione di ricerca criptata di Google), nella maggior parte dei casi i termini
+                             di ricerca non vengono inviati come parte dell'URL negli URL referrer. Questo comportamento può fare eccezione,
+                             ad esempio se utilizzi alcuni browser meno diffusi. Ulteriori informazioni sulla ricerca SSL sono disponibili qui.
+                             Le query di ricerca o le informazioni contenute nell'URL referrer potrebbero essere disponibili mediante Google Analytics
+                             o un'API (Application Programming Interface). Inoltre, gli inserzionisti potrebbero ricevere informazioni relative all' esatte
+                             parole chiave che hanno determinato il clic su un annuncio.",
+            ],        
         ];
     ?>
 
